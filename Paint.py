f100808b4e178076c896b48537b45949e91d2db9
Ho1ocron <108415700+Qloster@users.noreply.github.com>
2022-11-28 17:26:35 +0500

    Update Paint.py

--- Paint.py
+++ Paint.py
@@ -1,14 +1,18 @@
 import pygame
+from Line import Line
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((1550, 800))
 pygame.display.set_caption("My Game")
 running = True
+
 while running:
-    screen.fill((255,255,255))
+    screen.fill((0,0,0))
     pygame.display.flip()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #: Выход из окна
             running = False
-            
+            pygame.quit()
+            exit()
+        Line() #: Создание квадратов за мышкой
